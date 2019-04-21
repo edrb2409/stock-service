b1bf563b59f85cacc4204f12d3bd2dc54b52ff79
Daniel Ron <edaniel.ron@outlook.com>
2019-04-20 23:35:58 -0500

    Add statistics REST controller

---
+++ src/main/java/io/edrb/stockservice/controller/StatisticsController.java
@@ -0,0 +1,31 @@
+package io.edrb.stockservice.controller;
+
+import io.edrb.stockservice.model.StatisticRange;
+import io.edrb.stockservice.model.dto.statistics.StatisticResponseDTO;
+import io.edrb.stockservice.service.StatisticsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+
+@Slf4j
+@RestController("/statistics")
+public class StatisticsController {
+
+    private final StatisticsService service;
+
+    public StatisticsController(StatisticsService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticResponseDTO> getStatistics(@RequestParam StatisticRange time) {
+        log.debug("Requesting statistics for {}", time);
+
+        return ResponseEntity.ok(
+                service.getStatisticsFor(time, ZonedDateTime.now()));
+    }
+}
